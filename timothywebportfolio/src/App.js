b63e2ee7b85db9738b15b24cb40edab2e2e0dcdd
Timothy Reynoso <domex2467@gmail.com>
2020-11-02 12:55:44 -0500

    added navbar component and completed mvp for navbar.. also added components folder including projects, header and experiance components folder

--- timothywebportfolio/src/App.js
+++ timothywebportfolio/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import { Container } from '@material-ui/core';
+import Navbar from './Components/Navbar/Navbar'
 
 function App() {
   return (
-    <div className="App">
-      hello
+    <div>
+      <Container style={{ backgroundColor: '#553333', height: '100vh' }}>
+        <Navbar />
+        <h1>Timothy Reynoso</h1>
+      </Container>
     </div>
   );
 }
