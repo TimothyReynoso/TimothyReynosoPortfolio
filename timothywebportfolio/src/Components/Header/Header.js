0fca496691e247573540017762d9b910eec423f1
Timothy Reynoso <domex2467@gmail.com>
2020-11-07 18:08:56 -0500

    created the logo component with css also imported relivent images

--- timothywebportfolio/src/Components/Header/Header.js
+++ timothywebportfolio/src/Components/Header/Header.js
@@ -1,17 +1,19 @@
 import React from 'react'
 import './Header.css'
+import Logo from '../Logo/Logo'
 import { makeStyles } from '@material-ui/core/styles';
 import { Typography, Paper } from '@material-ui/core';
+import J from '../../Photos/J.png'
 
 const useStyles = makeStyles((theme) => ({
     root: {
       display: 'flex',
       flexWrap: 'wrap',
       '& > *': {
-        padding: theme.spacing(21),
+        // padding: theme.spacing(21),
         marginTop: theme.spacing(1),
         width: "100%",
-        height: theme.spacing('auto'),
+        height: theme.spacing(60),
       },
     },
   }));
@@ -22,19 +24,25 @@ function Header(){
 
     return (
         <div className={classes.root}>
-            <Paper id='paperDiv' elevation={3}> 
-                <Typography id='lineOne' variant="h5" component="h5">
-                    Hi, my name is
-                </Typography>
-                <Typography id='lineTwo'variant="h2" component="h2">
-                    Timothy Reynoso
-                </Typography>
-                <Typography id='lineThree' variant="h3" component="h3">
-                    I Build Things For The Web
-                </Typography>
-                <Typography id='lineFour' variant="h7" component="h7">
-                    I create quality web and software appliacations using modern techonogies and frameworks like JavaScript ES6, Python 3 & 2, React, Django, HTML 5 and CSS3
-                </Typography>
+            <Paper id='paperContainer' elevation={3}> 
+                <div id='paperDiv'>
+                    <Typography id='lineOne' variant="h5" component="h5">
+                        Hi, my name is
+                    </Typography>
+                    <Typography id='lineTwo'variant="h2" component="h2">
+                        Timothy Reynoso
+                    </Typography>
+                    <Typography id='lineThree' variant="h3" component="h3">
+                        I Build Things For The Web
+                    </Typography>
+                    <Typography id='lineFour' variant="h7" component="h7">
+                        I create quality web and software appliacations using modern techonogies and frameworks like JavaScript ES6, Python 3 & 2, React, Django, HTML 5 and CSS3
+                    </Typography>
+                </div>
+                <div>
+                    <Logo logo={J}/>
+                    
+                </div>
             </Paper>
         </div>
     )
