70a8a459a38298f8c773c897d3346aed1ea1012a
Timothy Reynoso <domex2467@gmail.com>
2020-11-07 15:49:13 -0500

    completed the mvp of the header component still needs further styling and another pic container

--- timothywebportfolio/src/App.js
+++ timothywebportfolio/src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import { Container } from '@material-ui/core';
 import Navbar from './Components/Navbar/Navbar'
+import Header from './Components/Header/Header'
 
 function App() {
   return (
     <div>
       <Container style={{ backgroundColor: '#553333', height: '100vh' }}>
         <Navbar />
-        <h1>Timothy Reynoso</h1>
+        <Header />
       </Container>
     </div>
   );
