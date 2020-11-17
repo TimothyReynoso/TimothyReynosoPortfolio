18daa393663eaa74b061bc014e4f3bed6ed5e479
Timothy Reynoso <domex2467@gmail.com>
2020-11-17 17:21:32 -0500

    added experiance card to portfolio

--- timothywebportfolio/src/Components/Body/Body.js
+++ timothywebportfolio/src/Components/Body/Body.js
@@ -1,12 +1,27 @@
 import React from 'react'
 import Project from '../Project/Project'
+import ReduxJS from '../../Photos/Redux.png'
+import Experiance from '../Experiance/Experiance'
+// import  from ''
+// import  from ''
+// import  from ''
+// import  from ''
+// import  from ''
 
 function Body(){
     const description = 'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica'
 
     return (
         <>
-            <Project project='Here' description={description}/>
+            <Experiance />
+            <Project
+            title="Alt Title" 
+            photo={ReduxJS} 
+            project='Todo-App' 
+            description={description}
+            gitlink='https://github.com/TimothyReynoso/react-todo-app'
+            weblink='https://react-todo-app-8160d.web.app/'
+            />
         </>
     )
 }
---
+++ timothywebportfolio/src/Components/Experiance/Experiance.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
+
+function Experiance() {
+  const classes = useStyles();
+  const bull = <span className={classes.bullet}>•</span>;
+
+  return (
+    <Card className={classes.root}>
+      <CardContent>
+        <Typography className={classes.title} color="textSecondary" gutterBottom>
+          Web
+        </Typography>
+        <Typography variant="h5" component="h2">
+          Experiance
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+          Developer
+        </Typography>
+        <Typography variant="body2" component="p">
+        · Highly experienced in building modern web applications based on React/Redux frameworks, JavaScript, Python, Django and other expertise.
+            <br />
+        · knowledge of Web Services like Google Firebase.
+            <br />
+        · Hand-on experience of unit-tests
+            <br />
+        · Infrastructure / DevOps experience (continuous integration setup, deploying and running applications in a test and production environment etc.)
+            <br />
+        · Solid understanding of the Agile development practices and the software development lifecycle.
+            <br />
+        · Experience with Continuous Integration (CI) and Continuous Delivery (CD) practices.
+            <br />        
+        · Thrives in a fast-paced, startup environment.
+            <br />
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  );
+}
+
+export default Experiance
