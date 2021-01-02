e09fbd5d4e77a9a4dd8c1d30a562720d4b667032
Timothy Reynoso <domex2467@gmail.com>
2021-01-02 18:51:49 -0500

    cleaned up more of the layout added 6 projects (need to complete filling out project information) also created a new component which displays a breaker/header called Breaker

--- timothywebportfolio/src/Components/Body/Body.js
+++ timothywebportfolio/src/Components/Body/Body.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import Project from '../Project/Project'
-import ReduxJS from '../../Photos/Redux.png'
+import FirebaseTodo from '../../Photos/firebase-todo.png'
+import KwitterLogin from '../../Photos/Kwitter-clone.png'
+import HaikuPhoto from '../../Photos/thelivinghaikupic.png'
 import Experiance from '../Experiance/Experiance'
+import Breaker from '../Breaker/Breaker'
+import './Body.css';
 // import  from ''
 // import  from ''
 // import  from ''
@@ -9,19 +13,69 @@ import Experiance from '../Experiance/Experiance'
 // import  from ''
 
 function Body(){
-    const description = 'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica'
+    const todoapp1 = "A React & Material UI based todo-app that uses firebase and firebase's firestore database to CREATE, RETREIVE, UPDATE and DELETE data."
+    const Kwitter = "Worked with a team of developers to build a React/Redux & Semantic UI based functional twitter clone with full user auth"
+
+    const HaikuDes = "Worked with a team of developers to build a public Haiku website using React, Semantic UI, Firebase and React-Router where anyone can share Haiku's with the world! Also allows for the randomization of all haiku's in the database"
 
     return (
         <>
             <Experiance />
-            <Project
-            title="Alt Title" 
-            photo={ReduxJS} 
-            project='Todo-App' 
-            description={description}
-            gitlink='https://github.com/TimothyReynoso/react-todo-app'
-            weblink='https://react-todo-app-8160d.web.app/'
-            />
+            <Breaker breaker='Front End Projects 🚀'/>
+            <div className='exContainer'>
+                <Project
+                title="TodoApp Photo" 
+                photo={FirebaseTodo} 
+                project='Todo-App' 
+                description={todoapp1}
+                gitlink='https://github.com/TimothyReynoso/react-todo-app'
+                weblink='https://react-todo-app-8160d.web.app/'
+                />
+                <Project
+                title="KwitterPhoto" 
+                photo={KwitterLogin} 
+                project='Kwitter' 
+                description={Kwitter}
+                gitlink='https://github.com/TimothyReynoso/Kwitter'
+                weblink='https://github.com/TimothyReynoso/Kwitter/blob/main/kwitter-clone-master/README.md'
+                />
+                <Project
+                title="The Living Haiku Photo" 
+                photo={HaikuPhoto} 
+                project='The Living Haiku' 
+                description={HaikuDes}
+                gitlink='https://github.com/TimothyReynoso/Kwitter'
+                weblink='https://the-living-haiku.web.app/'
+                />
+            </div>
+            <Breaker breaker='Back End Projects 🚀'/>
+            <div className='exContainer'>
+                <Project
+                title="Twitter Clone" 
+                photo={FirebaseTodo} 
+                project='Twitter Clone' 
+                description={todoapp1}
+                gitlink='https://github.com/TimothyReynoso/react-todo-app'
+                weblink='https://react-todo-app-8160d.web.app/'
+                />
+                <Project
+                title="SQL Manipulation" 
+                photo={KwitterLogin} 
+                project='SQL Manipulation' 
+                description={Kwitter}
+                gitlink='https://github.com/TimothyReynoso/Kwitter'
+                weblink='https://github.com/TimothyReynoso/Kwitter/blob/main/kwitter-clone-master/README.md'
+                />
+                <Project
+                title="Django Cowsay" 
+                photo={HaikuPhoto} 
+                project='Django Cowsay' 
+                description={HaikuDes}
+                gitlink='https://github.com/TimothyReynoso/Kwitter'
+                weblink='https://the-living-haiku.web.app/'
+                />
+            </div>
+           
         </>
     )
 }
