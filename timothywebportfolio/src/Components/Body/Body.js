22a8754956ef45f8d34974cc6422eb38bcbb37d7
Timothy Reynoso <domex2467@gmail.com>
2021-01-21 17:03:01 -0500

    added project to web portfolio

--- timothywebportfolio/src/Components/Body/Body.js
+++ timothywebportfolio/src/Components/Body/Body.js
@@ -6,6 +6,7 @@ import HaikuPhoto from '../../Photos/thelivinghaikupic.png'
 import TwitterClone from '../../Photos/Twitter-Clone.png'
 import SQLDEMO from '../../Photos/SQL-demo.png'
 import Cowsay from '../../Photos/cowsay.png'
+import Pyxz from '../../Photos/pyxzhome.png'
 import Experiance from '../Experiance/Experiance'
 import Breaker from '../Breaker/Breaker'
 import { makeStyles } from '@material-ui/core/styles';
@@ -36,9 +37,9 @@ function Body(){
     const HaikuDes = "Worked with a team of developers to build a public Haiku website using React, Semantic UI, Firebase and React-Router where anyone can share Haiku's with the world! Also allows for the randomization of all haiku's in the database"
     //Back-End
     const TwitterCloneDes = "A Django based twitter clone with full funcationallity including; following and unfollowing users, sign-up new users, count of followers and tweets, new tweets appears (after a page refresh) for all accounts that follow the user. If a user mentions another through the @ syntax; that user receives a notification (after page refresh), Users get redirected to /login/ when attempting to load the homepage without being logged in."
-    const SqlMani = "Wrote a python script to connect to a sqlite database, Using the Mimesis package for Python to generate 500 new user accounts, create 1000 tweets and 200 notifications than insert them into the database."
-    const CowDes = "Built a django server that passes user input to the operating system, runs a command, retrieves the output, and sends it back to the client. This was accomplished using the model to back up a copy of what people submit to the page, and had a second endpoint at /history where it listed the last ten things that were sent through the form. Also re-renders the homepage with a fresh form and the output from the Cowsay utility."
-
+    const SqlMani = "Wrote a Python script to connect to a sqlite database, Using the Mimesis package for Python to generate 500 new user accounts, create 1000 tweets and 200 notifications than insert them into the database."
+    const CowDes = "Built a Django server that passes user input to the operating system, runs a command, retrieves the output, and sends it back to the client. This was accomplished using the model to back up a copy of what people submit to the page, and had a second endpoint at /history where it listed the last ten things that were sent through the form. Also re-renders the homepage with a fresh form and the output from the Cowsay utility."
+    const PyxzDes = 'Built a social media Web-App MVP with a team of 4 in 2 weeks. Using Django, HTML, CSS and Materialize CSS Framework Built with full auth, login, sign-up, posting images, comments, tags to images, sub-categories with images based on created tags, follow users, users you follow page, unique randomization and stories (similar to Snapchat) that only last 24 HOURS!'
 
     const classes = useStyles();
     
@@ -82,6 +83,16 @@ function Body(){
             </div>
             <Breaker breaker='Back End Projects 🚀'/>
             <div className='exContainer'>
+                <div className='here'>
+                    <Project
+                    title="Pyxz Social Media ⭐️" 
+                    photo={Pyxz} 
+                    project='Pyxz Social Media ⭐️' 
+                    description={PyxzDes}
+                    gitlink='https://github.com/tiree24/Pyxz'
+                    weblink=''
+                    />
+                </div>
                 <div className='here'>
                     <Project
                     title="Twitter Clone" 
